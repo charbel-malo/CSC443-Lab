f7fc126b2891a73ec5e5783955fcf9108f674dcb
charbel-malo <68504022+charbel-malo@users.noreply.github.com>
2020-10-23 10:45:01 +0300

    Exercice 3

--- Lab6/js/main.js
+++ Lab6/js/main.js
@@ -23,4 +23,63 @@ $("#bordertoggle").click(function (){
         $(this).addClass("rounded");
     });;
     })
-        
+    function draw() {
+        var canvas = document.getElementById('myCanvas');
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+
+            var sWidth = canvas.width;
+            var sHeight = canvas.height;
+            var path=new Path2D();
+            ctx.beginPath();
+            path.moveTo((sWidth/2)+50,sHeight/2);
+            path.lineTo((sWidth/2),(sHeight/2)-50);
+            path.lineTo((sWidth/2)-50,sHeight/2);
+            path.lineTo((sWidth/2)+50,sHeight/2);
+            ctx.fillStyle = "red";
+            ctx.strokeStyle = "#000";
+            ctx.lineWidth = 5;
+            ctx.fill(path);
+            ctx.stroke(path);
+        }
+    }
+    function drawRotated(){
+        var canvas = document.getElementById('myCanvas');
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+            ctx.rotate(20 * Math.PI / 180);
+            var sWidth = canvas.width;
+            var sHeight = canvas.height;
+            var path=new Path2D();
+            ctx.beginPath();
+            path.moveTo((sWidth/2)+50,sHeight/2);
+            path.lineTo((sWidth/2),(sHeight/2)-50);
+            path.lineTo((sWidth/2)-50,sHeight/2);
+            path.lineTo((sWidth/2)+50,sHeight/2);
+            ctx.fillStyle = "red";
+            ctx.strokeStyle = "#000";
+            ctx.lineWidth = 5;
+            ctx.fill(path);
+            ctx.stroke(path);
+            ctx.rotate(-20 * Math.PI / 180);
+        }
+    }
+    function clear(){
+        var canvas = document.getElementById('myCanvas');
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+            var sWidth = canvas.width;
+            var sHeight = canvas.height;
+            ctx.clearRect(0,0,sWidth,sHeight);
+        }
+    }
+    
+$("#triangle").click(function (){
+    draw();
+});
+$("#rtriangle").click(function (){
+    drawRotated();
+});
+$("#clearcanv").click(function (){
+    clear();
+});
