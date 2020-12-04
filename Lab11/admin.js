3e3aafbba25211a4f01d383a649cd9fc66972e28
charbel-malo <68504022+charbel-malo@users.noreply.github.com>
2020-12-04 13:21:45 +0200

    Lab 11

--- Lab11/admin.js
+++ Lab11/admin.js
@@ -11,26 +11,23 @@ connection.onmessage=function(e){
     if(splitMsg[0]=="newconx")
     {
         let element= document.createElement("li");
-        element1.innerText=splitMsg[1];   //first name
-        element2.innerText=splitMsg[2];   //last name
-        element3.innerText=splitMsg[3];   //gender
-        element4.innerText=splitMsg[4];   //desc
-           let line= document.createElement("hr");
+        element1.innerText=splitMsg[1];   
+        element2.innerText=splitMsg[2]; 
+        element3.innerText=splitMsg[3];  
+        element4.innerText=splitMsg[4]; 
 
         element1.appendChild(document.createTextNode("First Name"));
-        document.querySelector("#AdminDisplay ul").appendChild(element1);
+        document.querySelector("#adminsection ul").appendChild(element1);
        
         element2.appendChild(document.createTextNode("Last Name"));
-        document.querySelector("#AdminDisplay ul").appendChild(element2);
+        document.querySelector("#adminsection ul").appendChild(element2);
         
         element3.appendChild(document.createTextNode("Gender"));
-        document.querySelector("#AdminDisplay ul").appendChild(element3);
+        document.querySelector("#adminsection ul").appendChild(element3);
          
         element4.appendChild(document.createTextNode("Description"));
-        document.querySelector("#AdminDisplay ul").appendChild(element4);
+        document.querySelector("#adminsection ul").appendChild(element4);
 
-
-        document.querySelector("#AdminDisplay ul").appendChild(line);
 
          
 
